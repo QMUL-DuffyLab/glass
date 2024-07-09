ec1003eda221a1abe61aebdda6aa5aaaf2e3de09
cuamll <callum.gray.10@ucl.ac.uk>
2024-07-09 15:44:10 +0100

    add protein to lattice generation, write MC step bit
    the MC step function's written but not tested; propose_move also written
    but not tested. for multiple types of protein it might be necessary to
    change how lattice_generation works and then have a separate function to
    distribute protein types; worry about that later. tomorrow, sort out the
    count histograms and simulation parameter stuff. nearly time to start
    separating out into module files as well, i think

--- notebook.jl
+++ notebook.jl
@@ -13,118 +13,11 @@ using Graphs
 # ╔═╡ 38ecc0bf-1fb3-4c5e-b7d9-5ec608277104
 using GraphPlot
 
-# ╔═╡ 18eb45f4-c158-4080-a0e9-dcc5e01c479e
-# this might be overkill, honestly, but we can use it to define arbitrary lattices
-struct LatticeParams
-	basis::Vector{Vector{Real}}
-	radius::Real
-	coordination::Integer
-end
-
-# ╔═╡ 19bbc9c5-18e8-43af-a307-90e133fe5200
-struct Lattice
-	params::LatticeParams
-	sites::Matrix{Real}
-	A::Matrix{Integer}
-	nn::Matrix{Integer}
-end
-
-# ╔═╡ 2aaf887d-6984-4403-a8f6-bc1d36710899
-neighbours(p1, p2, spacing) = isapprox(norm(p1 - p2), spacing, atol=1e-6)
-
-# ╔═╡ f3955bc5-f06f-4956-b5aa-1f7f0cd29f19
-"""
-generate the near-neighbour lattice vectors for a given set of
-lattice parameters. need to check this for honeycomb?
-"""
-function lattice_vectors(params)
-	nn = zeros(Real, 2, params.coordination)
-	for i=1:params.coordination
-		ϕ = 2π * i/params.coordination
-		r = [[cos(ϕ) sin(ϕ)]; [-sin(ϕ) cos(ϕ)]] * [params.radius, 0]
-		nn[:, i] = r
-	end
-	return nn
-end
-
-# ╔═╡ 29ce2bef-bd26-4d96-971f-fc44acb687eb
-"""
-generate a lattice of size nmax according to params.
-iterate over current sites, add if the current point is new.
-then loop again to add indices of neighbours.
-"""
-function lattice_generation(params::LatticeParams, nmax::Int)
-	lv = lattice_vectors(params)
-	nn = zeros(Integer, params.coordination, nmax)
-	spacing = norm(lv[:, 1])
-	sites = zeros(Real, 2, nmax)
-	sites[:, 1] = params.basis[1]
-	adj = zeros(Integer, nmax, nmax)
-	n_sites = 1
-	while n_sites < nmax
-		for site in eachcol(sites)
-			for n in eachcol(lv)
-				for b in params.basis
-					ri = site + n + b
-					add = true
-					for (i, site2) in enumerate(eachcol(sites[:, 1:n_sites]))
-						if isapprox(ri, site2, atol=1e-6)
-							add = false
-							break
-						end
-					end
-					if add && n_sites < nmax
-						n_sites += 1
-						sites[:, n_sites] = ri
-					end
-				end
-			end
-		end
-	end
-	# in theory there should be a way to generate neighbour data while
-	# adding sites, but in practice it's easier just to do this
-	for (i, s1) in enumerate(eachcol(sites))
-		k = 1
-		for (j, s2) in enumerate(eachcol(sites))
-			if neighbours([s1[1], s1[2]], [s2[1], s2[2]], spacing)
-				adj[i, j] = 1
-				nn[k, i] = j
-				k += 1
-			end
-		end
-	end
-	Lattice(params, sites, adj, nn)
-end
-
-# ╔═╡ d4fb1c0c-5fd5-4fb9-809a-741c1952095b
-function plot_lattice(l::Lattice)
-	g = SimpleGraph(l.A)
-	gplot(g, l.sites[1, :], l.sites[2, :])
-end
-
-# ╔═╡ 97eadd96-16a8-4b63-81a3-09cc59cecbf8
-# test the lattice generation
-begin
-	hex = LatticeParams([[0.0, 0.0]], 1.0, 6)
-	square = LatticeParams([[0.0, 0.0]], 1.0, 4)
-	line = LatticeParams([[0.0, 0.0]], 1.0, 2)
-	# honeycomb doesn't work atm. need to fix
-	honeycomb = LatticeParams([[0.0, 0.0], [0.0, -1.0]], 1.0, 6)
-
-	lattice = lattice_generation(hex, 200)
-	plot_lattice(lattice)
-end
-
-# ╔═╡ d0e27739-0ed1-4a30-b69e-2db62ca539ee
-struct State
-	name::String
-	decay::Real
-end
+# ╔═╡ 9f4eb629-f23f-4caa-a05b-c8618fce8b80
+using StatsBase
 
 # ╔═╡ cd0a1835-cee5-43d5-b2f4-b09309363b4c
 """
-now we need to define a protein.
-parameters here are as follows:
 - pigments = list of names of pigments
 - states = list of names of states on those pigments
 - ``n_p`` = `length(pigments)`
@@ -138,6 +31,7 @@ parameters here are as follows:
 - intra = matrix (``n_s``, ``n_s``) = decay rates on the diagonal,
   transfer rates between states on the off-diagonal
 - ann = matrix (``n_s``, ``n_s``) = annihilation rates between states
+- `which_ann` = matrix (``n_s``, ``n_s``) = which species (index) is annihilated
 - xsec = vector (``n_s``)? unsure about this yet. need it to get absorption
   and stimulated emission rates
 - emissive = bool vector to tell us which of the possible decay pathways are
@@ -166,8 +60,113 @@ struct Protein
 	hop::Vector{Real}
 	intra::Matrix{Real}
 	ann::Matrix{Real}
+	which_ann::Matrix{Integer}
 	xsec::Vector{Real}
 	emissive::Vector{Bool}
+end
+
+# ╔═╡ 18eb45f4-c158-4080-a0e9-dcc5e01c479e
+# this might be overkill, honestly, but we can use it to define arbitrary lattices
+struct LatticeParams
+	basis::Vector{Vector{Real}}
+	radius::Real
+	coordination::Integer
+end
+
+# ╔═╡ 0fe4a898-cf3c-4ed5-911c-361211ea7c9a
+struct Site
+	r::Vector{Real}
+	p::Protein
+end
+
+# ╔═╡ 19bbc9c5-18e8-43af-a307-90e133fe5200
+struct Lattice
+	params::LatticeParams
+	sites::Vector{Site}
+	A::Matrix{Integer}
+	nn::Matrix{Integer}
+end
+
+# ╔═╡ 2aaf887d-6984-4403-a8f6-bc1d36710899
+neighbours(p1, p2, spacing) = isapprox(norm(p1 - p2), spacing, atol=1e-6)
+
+# ╔═╡ f3955bc5-f06f-4956-b5aa-1f7f0cd29f19
+"""
+generate the near-neighbour lattice vectors for a given set of
+lattice parameters. need to check this for honeycomb?
+"""
+function lattice_vectors(params)
+	nn = zeros(Real, 2, params.coordination)
+	for i=1:params.coordination
+		ϕ = 2π * i/params.coordination
+		r = [[cos(ϕ) sin(ϕ)]; [-sin(ϕ) cos(ϕ)]] * [params.radius, 0]
+		nn[:, i] = r
+	end
+	return nn
+end
+
+# ╔═╡ 29ce2bef-bd26-4d96-971f-fc44acb687eb
+"""
+generate a lattice of size nmax according to params.
+iterate over current sites, add if the current point is new.
+then loop again to add indices of neighbours.
+"""
+function lattice_generation(params::LatticeParams, nmax::Int, proteins::Vector{Protein}, rho)
+	lv = lattice_vectors(params)
+	nn = zeros(Integer, params.coordination, nmax)
+	spacing = norm(lv[:, 1])
+	ps = sample(proteins, Weights(rho), nmax)
+	sites = Vector{Site}(undef, nmax)
+	sites[1] = Site(params.basis[1], ps[1])
+	adj = zeros(Integer, nmax, nmax)
+	n_sites = 1
+	while n_sites < nmax
+		for site in sites
+			for n in eachcol(lv)
+				for b in params.basis
+					ri = site.r + n + b
+					add = true
+					for (i, site2) in enumerate(sites[1:n_sites])
+						if isapprox(ri, site2.r, atol=1e-6)
+							add = false
+							break
+						end
+					end
+					if add && n_sites < nmax
+						n_sites += 1
+						sites[n_sites] = Site(ri, ps[n_sites])
+					end
+				end
+			end
+		end
+	end
+	# in theory there should be a way to generate neighbour data while
+	# adding sites, but in practice it's easier just to do this
+	for (i, s1) in enumerate(sites)
+		k = 1
+		for (j, s2) in enumerate(sites)
+			if neighbours(s1.r, s2.r, spacing)
+				adj[i, j] = 1
+				nn[k, i] = j
+				k += 1
+			end
+		end
+	end
+	Lattice(params, sites, adj, nn)
+end
+
+# ╔═╡ d4fb1c0c-5fd5-4fb9-809a-741c1952095b
+function plot_lattice(l::Lattice)
+	g = SimpleGraph(l.A)
+	xs = [s.r[1] for s in l.sites]
+	ys = [s.r[2] for s in l.sites]
+	gplot(g, xs, ys)
+end
+
+# ╔═╡ d0e27739-0ed1-4a30-b69e-2db62ca539ee
+struct State
+	name::String
+	decay::Real
 end
 
 # ╔═╡ 038c9e9c-5252-4801-9259-b669d42e2ccd
@@ -183,25 +182,23 @@ figure out at the start of the simulation which decays are emissive,
 then we can just index back into that.
 - `n`: matrix of occupations of each state on each site.
 - `i`: current site index.
-- `p`: which `protein` is on this site (atm we only have one type).
 - `l`: the lattice.
 - `ft`: the current intensity of the pulse (only considering excitation
   at one wavelength, at least currently.)
 """
-function propose_move(n::Matrix{Int}, i::Integer, p::Protein,
+function propose_move(n::Matrix{Int}, i::Integer,
 	l::Lattice, ft::Real)
 	
 	move_types = []
 	rate = 0.0
 	emissive = false
-	"""
-	# current intensity of pulse
-	# this can be done once in the MC loop and passed to the function
-	if t < length(pulse) * dt
-		ft = pulse[int(t / dt) + 1]
-	else 
-		ft = 0
-	end
+	p = l.sites[i].p
+	loss_index = 0
+	isi = i
+	fsi = i
+	ist = 0
+	fst = 0
+
 	"""
 	
 	if ft > 0
@@ -214,7 +211,7 @@ function propose_move(n::Matrix{Int}, i::Integer, p::Protein,
 		append!(move_types, "ann")
 	end
 	if length(move_types) == 0
-		return
+		return (0.0, "none", 0, 0, 0, 0, 0)
 	end
 
 	# pick a type of move
@@ -222,10 +219,14 @@ function propose_move(n::Matrix{Int}, i::Integer, p::Protein,
 	if mt == "gen"
 		s = rand(p.xsec[p.xsec .> 0.0])
 		rate = ft * p.xsec[s] * (p.n_tot[p.ps[s]] - n[i, s]) / p.n_tot[p.ps[s]]
+		ist = s
+		fst = s
 	elseif mt == "se"
 		s = rand(p.xsec[p.xsec .> 0.0])
 		rate = ft * p.xsec[s] * n[i, s] / p.n_tot[p.ps[s]]
 		loss_index = s
+		ist = s
+		fst = s
 	elseif mt == "hop"
 		s = rand(1:p.nₛ)
 		nn = rand(l.sites[i].nn)
@@ -235,19 +236,26 @@ function propose_move(n::Matrix{Int}, i::Integer, p::Protein,
 			rate *= (n[i, s] * (p.n_thermal[p.ps[s]] - n[nn, s])) / 
 			((n[nn, s] + 1) * (p.n_thermal[p.ps[s]] - n[i, s] + 1))
 		end
+		ist = s
+		fsi = nn
+		fst = s
 	elseif mt == "transfer"
 		si = rand(1:p.nₛ)
 		sf = rand(1:p.nₛ)
 		while sf == si
-			# gotta be intracomplex transfer between two species
+			# this would be a decay process - reroll
 			sf = rand(1:p.nₛ)
 		end
 		rate = n[i, si] * intra[si, sf] # is this right? check
+		ist = si
+		fst = sf
 	elseif mt == "decay"
 		s = rand(1:p.nₛ)
 		rate = n[i, si] * intra[si, si]
 		emissive = p.emissive[s]
 		loss_index = s + p.nₛ
+		ist = s
+		fst = s
 	elseif mt == "ann"
 		si = rand(1:p.nₛ)
 		sf = rand(1:p.nₛ)
@@ -258,36 +266,113 @@ function propose_move(n::Matrix{Int}, i::Integer, p::Protein,
 			neff = n[i, si] + n[i, sf]
 			g = ann[si, sf] * (neff * (neff - 1)) / 2.0
 		end
-		# this isn't right yet - need to know which gets annihilated
-		# in order to know which column to increment
+		# we're gonna have one column for every possible annihilation pair
 		loss_index = sf + (2 + si) * p.nₛ
+		ist = si
+		fst = sf
 	end
-	return (rate, move_type, loss_index, emissive)
+	return (rate, move_type, loss_index, isi, ist, fsi, fst)
 end
 
 # ╔═╡ 652428a5-4618-4986-bf87-cf7819b4359e
-""" carry out an accepted move """
-function move!(n, move_type, i, x, j=i, y=x, which_ann=1)
+""" 
+carry out an accepted move.
+- `n`: matrix of occupations of states on each site
+- `move_type`: string to tell us what the effect of the move is
+- `isi`: index of initial site
+- `ist`: index of initial state on `sites[isi]`
+- `fsi`: index of final site
+- `fst`: index of final state on `sites[fsi]`
+- `which_ann`: integer to tell us which of the states loses population.
+  Check which site this corresponds to and decrement that one.
+"""
+function move!(n, move_type, isi, ist, 
+	fsi, fst, which_ann)
 	if move_type == "hop"
-		n[i, x] -= 1
-		n[j, y] += 1
+		n[isi, ist] -= 1
+		n[fsi, fst] += 1
 	elseif move_type == "decay"
-		n[i, x] -= 1
+		n[isi, ist] -= 1
 	elseif move_type == "gen"
-		n[i, x] += 1
+		n[isi, ist] += 1
 	elseif move_type == "ann"
-		if which_ann == 1
-			n[i, x] -= 1
+		if which_ann == ist
+			n[isi, ist] -= 1
 		else
-			n[j, y] -= 1
+			n[fsi, fst] -= 1
 		end
 	end
 end
 
-# ╔═╡ f8464bc1-eeee-4bea-a5d0-291045c1b2da
+# ╔═╡ 9fbd7017-7bbb-4057-a609-1b079afb4207
+"""
+one monte carlo step. attempt one move on each site on average;
+`propose_move` picks a possible move wherever possible.
+once a move's been picked, run it through Metropolis; if it's accepted,
+carry it out and increment the histogram iff 1.) an excitation's been lost
+and 2.) we are currently binning decays.
+"""
+function mc_step!(l::Lattice, n::Matrix{Integer}, t::Real, dt::Real,
+	bin::Bool, counts::Matrix{Integer}, binwidth::Real)
+	
+	# current intensity of pulse
+	if t < length(pulse) * dt
+		ft = pulse[int(t / dt) + 1]
+	else 
+		ft = 0
+	end
+	
+	for i=1:length(l)
+		s = rand(length(l)) # pick a random site
+		(rate, move_type, loss_index, isi, ist, fsi, fst) = propose_move(n, s, l, ft)
+		if move_type == "none"
+			continue # no possible moves here
+		end
+		
+		# metropolis
+		prob = rate * exp(-1.0 * rate * dt)
+		if rand() < prob
+			# this needs sorting lol - add i, x, j, y to propose_move
+			move!(n, move_type, isi, ist, fsi, fst, l.sites[s].p.which_ann[ist, fst])
+			if bin && loss_index > 0
+				# increment the correct column of the histogram
+				counts[loss_index, floor(t / binwidth) + 1] += 1
+			end
+		end
+	end
+	t += dt
+end
+
+# ╔═╡ 97eadd96-16a8-4b63-81a3-09cc59cecbf8
+# test the lattice generation
 begin
+	hex = LatticeParams([[0.0, 0.0]], 1.0, 6)
+	square = LatticeParams([[0.0, 0.0]], 1.0, 4)
+	line = LatticeParams([[0.0, 0.0]], 1.0, 2)
+	# honeycomb doesn't work atm. need to fix
+	honeycomb = LatticeParams([[0.0, 0.0], [0.0, -1.0]], 1.0, 6)
+	
+	lh2 = Protein(["BChl", "Car"],
+		["BChl_S", "BChl_T", "Car_T"],
+		2, 3, [1, 1, 2], 
+		[[false false true]; [false false true]; [true true false]],
+		[20, 4], # check this
+		[20, 4],
+		[10e-12, 0.0, 0.0],
+		[[1e-9 1e-7 0.0]; [0.0 1e-7 1e-7]; [0.0 0.0 1e-7]],
+		[[16e-9 16e-9 0.0]; [16e-9 16e-9 0.0]; [0.0 0.0 16e-9]],
+		[[1 1 0]; [1 1 0]; [0 0 3]],
+		[1e-20, 0.0, 0.0],
+		[true, false, false]
+	)
+
+	lattice = lattice_generation(hex, 200, [lh2], [1.0])
+	plot_lattice(lattice)
+
 	xsec = [1.0E-20, 0.0, 2E-20, 0.0, 0.0]
 	print(rand(xsec[xsec .> 0.0]))
+
+	
 end
 
 # ╔═╡ 0afa851d-c9cc-4815-8b16-3197bf6670ba
@@ -398,10 +483,12 @@ PLUTO_PROJECT_TOML_CONTENTS = """
 GraphPlot = "a2cc645c-3eea-5389-862e-a155d0052231"
 Graphs = "86223c79-3864-5bf0-83f7-82e725a168b6"
 LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+StatsBase = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
 
 [compat]
 GraphPlot = "~0.6.0"
 Graphs = "~1.9.0"
+StatsBase = "~0.34.3"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -410,7 +497,7 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.10.4"
 manifest_format = "2.0"
-project_hash = "a0bd92f3b844cbfd97daa9564f0fad17013832bb"
+project_hash = "2f1e51918f611b1db585c2569f85ba4794558004"
 
 [[deps.ArnoldiMethod]]
 deps = ["LinearAlgebra", "Random", "StaticArrays"]
@@ -457,6 +544,11 @@ git-tree-sha1 = "bf6570a34c850f99407b494757f5d7ad233a7257"
 uuid = "a81c6b42-2e10-5240-aca2-a61377ecd94b"
 version = "0.9.5"
 
+[[deps.DataAPI]]
+git-tree-sha1 = "abe83f3a2f1b857aac70ef8b269080af17764bbe"
+uuid = "9a962f9c-6df0-11e9-0e5d-c546b8b5ee8a"
+version = "1.16.0"
+
 [[deps.DataStructures]]
 deps = ["Compat", "InteractiveUtils", "OrderedCollections"]
 git-tree-sha1 = "1d0a14036acb104d9e89698bd408f63ab58cdc82"
@@ -476,6 +568,12 @@ version = "1.9.1"
 [[deps.Distributed]]
 deps = ["Random", "Serialization", "Sockets"]
 uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
+
+[[deps.DocStringExtensions]]
+deps = ["LibGit2"]
+git-tree-sha1 = "2fb1e02f2b635d0845df5d7c167fec4dd739b00d"
+uuid = "ffbed154-4ef7-542d-bbb7-c09d3a79fcae"
+version = "0.9.3"
 
 [[deps.FixedPointNumbers]]
 deps = ["Statistics"]
@@ -504,6 +602,11 @@ version = "0.1.5"
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
 
+[[deps.IrrationalConstants]]
+git-tree-sha1 = "630b497eafcc20001bba38a4651b327dcfc491d2"
+uuid = "92d709cd-6900-40b7-9082-c6be49f344b6"
+version = "0.2.2"
+
 [[deps.IterTools]]
 git-tree-sha1 = "42d5f897009e7ff2cf88db414a389e5ed1bdd023"
 uuid = "c8e1da08-722c-5040-9ed9-7db0dc04731e"
@@ -515,12 +618,42 @@ git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
 uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
 version = "0.21.4"
 
+[[deps.LibGit2]]
+deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibGit2_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
+uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
+version = "1.6.4+0"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.11.0+1"
+
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
 
 [[deps.LinearAlgebra]]
 deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
 uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.LogExpFunctions]]
+deps = ["DocStringExtensions", "IrrationalConstants", "LinearAlgebra"]
+git-tree-sha1 = "a2d09619db4e765091ee5c6ffe8872849de0feea"
+uuid = "2ab3a3ac-af41-5b50-aa03-7779005ae688"
+version = "0.3.28"
+
+    [deps.LogExpFunctions.extensions]
+    LogExpFunctionsChainRulesCoreExt = "ChainRulesCore"
+    LogExpFunctionsChangesOfVariablesExt = "ChangesOfVariables"
+    LogExpFunctionsInverseFunctionsExt = "InverseFunctions"
+
+    [deps.LogExpFunctions.weakdeps]
+    ChainRulesCore = "d360d2e6-b24c-11e9-a2a3-2a2ae2dbcce4"
+    ChangesOfVariables = "9e997f8a-9a97-42d5-a9f1-ce6bfc15e2c0"
+    InverseFunctions = "3587e190-3f89-42d0-90ee-14403ec27112"
 
 [[deps.MacroTools]]
 deps = ["Markdown", "Random"]
@@ -532,13 +665,28 @@ version = "0.5.13"
 deps = ["Base64"]
 uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.2+1"
+
 [[deps.Measures]]
 git-tree-sha1 = "c13304c81eec1ed3af7fc20e75fb6b26092a1102"
 uuid = "442fdcdd-2543-5da2-b0f3-8c86c306513e"
 version = "0.3.2"
 
+[[deps.Missings]]
+deps = ["DataAPI"]
+git-tree-sha1 = "ec4f7fbeab05d7747bdf98eb74d130a2a2ed298d"
+uuid = "e1d29d7a-bbdc-5cf2-9ac0-f12de2c33e28"
+version = "1.2.0"
+
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
@@ -607,6 +755,12 @@ version = "0.9.4"
 [[deps.Sockets]]
 uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
 
+[[deps.SortingAlgorithms]]
+deps = ["DataStructures"]
+git-tree-sha1 = "66e0a8e672a0bdfca2c3f5937efb8538b9ddc085"
+uuid = "a2af1166-a08f-5f64-846c-94a0d3cef48c"
+version = "1.2.1"
+
 [[deps.SparseArrays]]
 deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
 uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
@@ -636,6 +790,18 @@ deps = ["LinearAlgebra", "SparseArrays"]
 uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 version = "1.10.0"
 
+[[deps.StatsAPI]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "1ff449ad350c9c4cbc756624d6f8a8c3ef56d3ed"
+uuid = "82ae8749-77ed-4fe6-ae5f-f523153014b0"
+version = "1.7.0"
+
+[[deps.StatsBase]]
+deps = ["DataAPI", "DataStructures", "LinearAlgebra", "LogExpFunctions", "Missings", "Printf", "Random", "SortingAlgorithms", "SparseArrays", "Statistics", "StatsAPI"]
+git-tree-sha1 = "5cf7606d6cef84b543b483848d4ae08ad9832b21"
+uuid = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
+version = "0.34.3"
+
 [[deps.SuiteSparse_jll]]
 deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
 uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
@@ -663,18 +829,20 @@ version = "5.8.0+1"
 # ╠═8309ef8f-23d5-49a6-9cc2-cb210302c555
 # ╠═d536667a-0b7a-45ce-9471-5fe8d1b654d1
 # ╠═38ecc0bf-1fb3-4c5e-b7d9-5ec608277104
+# ╠═9f4eb629-f23f-4caa-a05b-c8618fce8b80
+# ╠═cd0a1835-cee5-43d5-b2f4-b09309363b4c
 # ╠═18eb45f4-c158-4080-a0e9-dcc5e01c479e
+# ╠═0fe4a898-cf3c-4ed5-911c-361211ea7c9a
 # ╠═19bbc9c5-18e8-43af-a307-90e133fe5200
 # ╠═2aaf887d-6984-4403-a8f6-bc1d36710899
 # ╠═f3955bc5-f06f-4956-b5aa-1f7f0cd29f19
 # ╠═29ce2bef-bd26-4d96-971f-fc44acb687eb
 # ╠═d4fb1c0c-5fd5-4fb9-809a-741c1952095b
-# ╠═97eadd96-16a8-4b63-81a3-09cc59cecbf8
 # ╠═d0e27739-0ed1-4a30-b69e-2db62ca539ee
-# ╠═cd0a1835-cee5-43d5-b2f4-b09309363b4c
 # ╠═0afa851d-c9cc-4815-8b16-3197bf6670ba
 # ╠═038c9e9c-5252-4801-9259-b669d42e2ccd
 # ╠═652428a5-4618-4986-bf87-cf7819b4359e
-# ╠═f8464bc1-eeee-4bea-a5d0-291045c1b2da
+# ╠═9fbd7017-7bbb-4057-a609-1b079afb4207
+# ╠═97eadd96-16a8-4b63-81a3-09cc59cecbf8
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
