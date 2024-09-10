b451d3252befe910e27af9049809c28cffa46487
cuamll <callum.gray.10@ucl.ac.uk>
2024-09-10 13:31:11 +0100

    continue with fit. tail fit starting to work now

--- src/ReconvolutionFits.jl
+++ src/ReconvolutionFits.jl
@@ -5,7 +5,7 @@ using PyPlot, Symbolics, FFTW, LsqFit, DelimitedFiles
 function multi_exp(x, p)
     # expects [A1, A2,...τ1, τ2...]
     n_exp = length(p)÷2
-    sum([p[i] * exp.(-x ./ p[i + n_exp]) for i=1:n_exp])
+    sum([p[i] * exp.(-x .* (1.0 / p[i + n_exp])) for i=1:n_exp])
 end
 
 function convol(x, h)
@@ -18,16 +18,17 @@ end
 
 function plot_fit(fit, xyn, outfile)
     x = xyn[:, 1]
+    x_ns = x .* 1e9
     y = xyn[:, 3]
-    yfit = multi_exp(x, fit.param)
+    yfit = multi_exp(x_ns, fit.param)
     fig, ax = plt.subplots(figsize=(12,8))
-    plot(x, y, label="data")
-    plot(x, yfit, label="fit")
-    ax.set_ylim(1, 2.0) # normalised count max to 1
+    plot(x_ns, y, label="data")
+    plot(x_ns, yfit, label="fit")
+    ax.set_ylim(1e-5, 2.0) # normalised count max to 1
     ax.set_yscale("log")
     plt.grid(visible=true)
     ax.legend()
-    ax.set_xlabel("time (s)")
+    ax.set_xlabel("time (ns)")
     ax.set_ylabel("counts")
     savefig(outfile)
     plt.close()
@@ -44,27 +45,29 @@ function fit(filename, τᵢ, irf_file, pulse_fwhm, pulse_peak)
     max_time = bincounts[argmax(ec), 1]
     xyn = hcat(bincounts[:, 1], ec, ec / maximum(ec))
     tail = xyn[xyn[:, 1] .>= max_time, :]
-    tail_σ = zeros(Float64, length(xyn[:, 2])) 
+    tail_σ = zeros(Float64, length(tail[:, 2])) 
     for i = 1:length(tail_σ)
-        count = xyn[i, 2] > 0 ? xyn[i, 2] : one(eltype(tail_σ))
+        count = tail[i, 2] > 0 ? tail[i, 2] : one(eltype(tail_σ))
         tail_σ[i] = sqrt((1.0 / count) + (1.0 / maximum(ec)))
     end
+    wt = 1.0 ./ tail_σ
     n_exp = length(τᵢ)
-    p0 = [[1.0/n_exp for i=1:n_exp]..., τᵢ...]
+    p0 = [[1.0/n_exp for i=1:n_exp]..., τᵢ .* 1e9...]
     lbs = [0. for i=1:n_exp*2]
     ubs = [Inf for i=1:n_exp*2]
-    x = xyn[:, 1] .- max_time
-    y = xyn[:, 3]
-    fit_res = curve_fit(multi_exp, x, y, p0, lower=lbs, upper=ubs)
+    # x = 1e9 .* (tail[:, 1] .- max_time)
+    x = 1e9 .* (tail[:, 1])
+    y = tail[:, 3]
+    fit_res = curve_fit(multi_exp, x, y, wt, p0, lower=lbs, upper=ubs)
     fit_error = stderror(fit_res)
     margin_of_error = margin_error(fit_res, 0.05)
 
-    println(fit_res.param)
-    println(fit_error)
-    println(margin_of_error)
+    println("best fit: $(fit_res.param)")
+    println("errors: $(fit_error)")
+    println("margin: $(margin_of_error)")
     outfile = joinpath(dirname(filename), "fit.pdf")
     println(outfile)
-    plot_fit(fit_res, xyn, outfile)
+    plot_fit(fit_res, tail, outfile)
 
 end
 
