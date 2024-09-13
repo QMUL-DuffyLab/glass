30959a6baafb260ee71ff683c79b4c6f9781bcfb
cuamll <callum.gray.10@ucl.ac.uk>
2024-09-13 17:47:52 +0100

    fix incorrect units in tau in single_core.jl

--- single_core.jl
+++ single_core.jl
@@ -8,6 +8,6 @@ hist_files = Simulations.run("parameters.json", 0, outpath)
 
 for file in hist_files
     irf_file = joinpath(dirname(file), "pulse.txt")
-    τᵢ= [1.1]
+    τᵢ= [1.1e-9]
     ReconvolutionFits.fit(file, τᵢ, irf_file)
 end
