3a8fc08da7f4784003eefe5c5b0ac8dd036b0da3
cuamll <callum.gray.10@ucl.ac.uk>
2024-10-10 11:35:27 +0100

    push to gitlab, mpi comment

--- mpi.jl
+++ mpi.jl
@@ -16,4 +16,10 @@ end
 args = parse_cmds()
 addprocs(args["n_procs"])
 
-
+"""
+mpi broadcast the name of the parameter file?
+then get each proc to print out the simulation params
+etc and check it's in the right form?
+then check the sum over histogram in the code and
+figure out how to do that in the reduce call
+"""
